b540729527e4a2d29e0209f95aaa1860567f4292
unknown <hdtmrsame8@gmail.com>
2025-07-29 21:59:27 -0300

    Exercicio tipos primitivos

---
+++ Aula14/TiposPrimitivos.java
@@ -0,0 +1,19 @@
+/*
+Pratica
+
+Crie variaveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+Eu <nome>, morando no endereço <endereço>,
+confirmo que recebi o salário de <salário>, na data <data>
+*/
+
+public class TiposPrimitivos {
+	public static void main(String[] args) {
+	String nome = "André";
+	String endereco = "R Feliciano de Mendonça 160";
+	double salario = 1200;
+	String data = "29/07/2025";
+
+	System.out.println("Eu "+nome+" morando no endereço "+endereco+" confirmo que recebi o "+salario+", na data "+data);
+	}
+}
